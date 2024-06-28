dbb7e61220976affbf2ed99b773bf092cf8d2755
babydev <babydev.weangkom@gmail.com>
2024-06-28 22:04:57 +0700

    Update LineFix.tsx

--- app/components/HomeNew/(NewTemplate)/LineFix.tsx
+++ app/components/HomeNew/(NewTemplate)/LineFix.tsx
@@ -7,7 +7,7 @@ const LineFix = () => {
   return (
     <div className=" fixed bottom-4 right-5 sm:bottom-0 md:bottom-4  z-[9999999]">
       <Link
-        href="https://line.me/R/ti/p/@361nxolu?from=page&searchId=361nxolu"
+        href="https://lin.ee/8e7KCcu"
         target="__blank"
       >
         <div className="flex items-center gap-2 rounded-2xl bg-[#06C755] hover:bg-green-600 p-4 text-white">
